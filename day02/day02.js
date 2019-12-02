f4038b19108966e8628a08de9321f3ca1c7f97ae
Cort Stratton <cort@unity3d.com>
2019-12-02 13:46:08 -0800

    Day 2 solution

--- day02/day02.js
+++ day02/day02.js
@@ -1,68 +1,110 @@
-var aoc01_1 = function() {
-    "use strict"
-    var testCase = function(func, input, expected) {
-        var actual = func(input);
-        console.assert(actual === expected, "%s(%d) is %d (expected %d)", func.name, input, actual, expected);
-    }
+let aoc02 = function() {
+    "use strict";
 
-    var processIntcodeProgram = function(pgm) {
-        return pgm;
-    }
+    let compareArrays = function(arr1, arr2) {
+        return arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
+    };
     
-    var processFile1 = function(inElem, outElem) {
-        var firstFile = document.querySelector(inElem).files[0];
-        var reader = new FileReader();
-        reader.onload = (event) => {
-            const file = event.target.result;
-            const allLines = reader.result.split(/\r\n|\n/);
+    let testCase = function(func, input, expected, compareFunc) {
+        let actual = func(input);
+        console.assert(compareFunc(actual, expected), "%s(%s) is %s (expected %s)", func.name, input, actual, expected);
+    };
 
-            var re = /^(\d+)$/;
-            allLines.forEach( (line) => {
-                //console.log(line);
-                var match = line.match(re);
-                if (match) {
-                }
-            });
-            
-            document.querySelector(outElem).innerHTML = sum;
+    let checkAddr = function(pgm, ip, addr) {
+        if (addr < 0 || addr >= pgm.length) {
+            console.log("ERROR: out of bounds addr (%d)", addr);
+            console.log(pgm);
+            console.log("ip=%d", ip);
+            throw {
+                pgm: pgm,
+                ip: ip,
+                msg: "ERROR: out of bounds addr (" + addr + ")",
+            };
+        }
+    };
+    
+    let processIntcodeProgram = function(pgm) {
+        let ip = 0;
+        do {
+            checkAddr(pgm, ip, ip);
+            if (pgm[ip] === 1) {
+                let src1 = pgm[ip+1];
+                let src2 = pgm[ip+2];
+                let dst = pgm[ip+3];
+                checkAddr(pgm, ip, src1);
+                checkAddr(pgm, ip, src2);
+                checkAddr(pgm, ip, dst);
+                pgm[dst] = pgm[src1] + pgm[src2];
+                ip += 4;
+            } else if (pgm[ip] === 2) {
+                let src1 = pgm[ip+1];
+                let src2 = pgm[ip+2];
+                let dst = pgm[ip+3];
+                checkAddr(pgm, ip, src1);
+                checkAddr(pgm, ip, src2);
+                checkAddr(pgm, ip, dst);
+                pgm[dst] = pgm[src1] * pgm[src2];
+                ip += 4;
+            } else if (pgm[ip] === 99) {
+                break;
+            } else {
+                throw {
+                    pgm: pgm,
+                    ip: ip,
+                    msg: "ERROR: Unsupported opcode (" + pgm[ip] + ")",
+                };
+            }
+        } while (true);
+        return pgm;
+    };
+    
+    let processFile = function(inElem, callback, outElem) {
+        let firstFile = document.querySelector(inElem).files[0];
+        let reader = new FileReader();
+        reader.onload = (event) => {
+            //const file = event.target.result;
+            const allInts = reader.result.split(/,/u).map(elem => parseInt(elem));
+
+            let result = callback(allInts);
+
+            document.querySelector(outElem).innerHTML = result;
         };
         reader.onerror = (event) => {
             alert(event.target.error.name);
         };
         reader.readAsText(firstFile);
-    }
+    };
 
-    var processFile2 = function(inElem, outElem) {
-        var firstFile = document.querySelector(inElem).files[0];
-        var reader = new FileReader();
-        reader.onload = (event) => {
-            const file = event.target.result;
-            const allLines = reader.result.split(/\r\n|\n/);
-
-            var re = /^(\d+)$/;
-            allLines.forEach( (line) => {
-                //console.log(line);
-                var match = line.match(re);
-                if (match) {
-                }
-            });
-            
-            document.querySelector(outElem).innerHTML = sum;
-        };
-        reader.onerror = (event) => {
-            alert(event.target.error.name);
-        };
-        reader.readAsText(firstFile);
-    }
-
-    testCase(processIntcodeProgram, [1,9,10,3,2,3,11,0,99,30,40,50], [3500,9,10,70,2,3,11,0,99,30,40,50]);
-    testCase(processIntcodeProgram, [1,0,0,0,99], [2,0,0,0,99]);
-    testCase(processIntcodeProgram, [2,3,0,3,99], [2,3,0,6,99]);
-    testCase(processIntcodeProgram, [2,4,4,5,99,0], [2,4,4,5,99,9801]);
-    testCase(processIntcodeProgram, [1,1,1,4,99,5,6,0,99], [30,1,1,4,2,5,6,0,99]);
+    testCase(processIntcodeProgram, [1,9,10,3,2,3,11,0,99,30,40,50,], [3500,9,10,70,2,3,11,0,99,30,40,50,], compareArrays);
+    testCase(processIntcodeProgram, [1,0,0,0,99,], [2,0,0,0,99,], compareArrays);
+    testCase(processIntcodeProgram, [2,3,0,3,99,], [2,3,0,6,99,], compareArrays);
+    testCase(processIntcodeProgram, [2,4,4,5,99,0,], [2,4,4,5,99,9801,], compareArrays);
+    testCase(processIntcodeProgram, [1,1,1,4,99,5,6,0,99,], [30,1,1,4,2,5,6,0,99,], compareArrays);
     
     return {
-        processFile1: processFile1,
-        processFile2: processFile2,
+        processFile: processFile,
+        processIntcodeProgramAndReturnElemZero: (pgm) => {
+            pgm[1] = 12;
+            pgm[2] = 2;
+            let pout = processIntcodeProgram(pgm);
+            return pout[0];
+        },
+        processIntcodeProgramWithGoal: (pgm) => {
+            let goal = 19690720;
+            let noun = 0;
+            let verb = 0;
+            for(noun=0; noun<=99; ++noun) {
+                for(verb=0; verb<=99; ++verb) {
+                    let mem = pgm.slice();
+                    mem[1] = noun;
+                    mem[2] = verb;
+                    let mout = processIntcodeProgram(mem);
+                    if (mout[0] === goal) {
+                        return 100 * noun + verb;
+                    }
+                }
+            }
+            return "No such luck :(";
+        },
     };
 }();
