5d5abb37bc7ac61d1b8656c575d4e2285c9ae44a
Cort <1944792+cdwfs@users.noreply.github.com>
2019-12-02 19:58:53 -0800

    missing semicolon

--- day02/day02.js
+++ day02/day02.js
@@ -85,7 +85,7 @@ let aoc02 = function() {
         testCase(processIntcodeProgram, [2,4,4,5,99,0,], [2,4,4,5,99,9801,], compareArrays);
         testCase(processIntcodeProgram, [1,1,1,4,99,5,6,0,99,], [30,1,1,4,2,5,6,0,99,], compareArrays);
         document.querySelector("#testResults").innerHTML = "All tests passed!";
-    }
+    };
     
     return {
         processFile: processFile,
