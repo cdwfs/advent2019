05d6795e2f2554e1158614807950e4000de4565c
Cort Stratton <cort@unity3d.com>
2019-12-04 13:06:11 -0800

    Day 4 solution

--- day04/day04.js
+++ day04/day04.js
@@ -2,14 +2,60 @@
 let aoc04 = function() {
     "use strict";
 
-    let isViablePasscode = function(passcode) {
-        return false;
+    let isViablePasscode1 = function(passcode) {
+        const asStr = passcode.toString();
+        if (asStr.length !== 6) {
+            return false;
+        }
+        let i = 0;
+        let hasDouble = false;
+        for(i=0; i<asStr.length-1; ++i) {
+            if (asStr[i+1] < asStr[i]) {
+                return false; // digits must never decrease
+            }
+            if (asStr[i] === asStr[i+1]) {
+                hasDouble = true; // must have at least one duplicate of the same digit
+            }
+        }
+        if (!hasDouble) {
+            return false;
+        }
+        return true;
     };
     
-    let countViablePasscodes = function(range) {
-        const rangeMin = range.min;
-        const rangeMax = range.max;
-        return 0;
+    let isViablePasscode2 = function(passcode) {
+        const asStr = passcode.toString();
+        if (asStr.length !== 6) {
+            return false;
+        }
+        let i = 0;
+        let hasDouble = false;
+        for(i=0; i<asStr.length-1; ++i) {
+            if (asStr[i+1] < asStr[i]) {
+                return false; // digits must never decrease
+            }
+            if ((asStr[i] === asStr[i+1]) &&
+                (asStr[i-1] || "A") !== asStr[i] &&
+                (asStr[i+2] || "A") !== asStr[i]) {
+                hasDouble = true; // must have at least one duplicate of the same digit that is *not* part of a larger group
+            }
+        }
+        if (!hasDouble) {
+            return false;
+        }
+
+        return true;
+    };
+    
+    let countViablePasscodes = function(range, isViableFunc) {
+        let i=0;
+        let count = 0;
+        for(i=range.min; i<=range.max; ++i) {
+            if (isViableFunc(i)) {
+                count += 1;
+            }
+        }
+        return count;
     };
     
     let processFile = function(inElem, callback, outElem) {
@@ -20,12 +66,15 @@ let aoc04 = function() {
             const allLines = reader.result.split(/\r\n|\n/u);
             const re = /(\d{6})-(\d{6})/u;
             const match = allLines[0].match(re);
-            let count = 0;
+            let result = 0;
             if (match) {
-                count = callback({min: parseInt(match[1]), max: parseInt(match[2])});
+                result = callback({min: parseInt(match[1]), max: parseInt(match[2])});
             }
-
-            document.querySelector(outElem).innerHTML = count;
+            if (result.actual === result.expected) {
+                document.querySelector(outElem).innerHTML = `${result.actual} (Correct!)`;
+            } else {
+                document.querySelector(outElem).innerHTML = `${result.actual} (ERROR: expected ${result.expected})`;
+            }
         };
         reader.onerror = (event) => {
             alert(event.target.error.name);
@@ -35,9 +84,13 @@ let aoc04 = function() {
 
     window.onload = function() {
         // Part 1 tests
-        aoc.testCase(isViablePasscode, 111111, true);
-        aoc.testCase(isViablePasscode, 223450, false);
-        aoc.testCase(isViablePasscode, 123789, false);
+        aoc.testCase(isViablePasscode1, [111111,],  true);
+        aoc.testCase(isViablePasscode1, [223450,], false);
+        aoc.testCase(isViablePasscode1, [123789,], false);
+        // Part 2 tests
+        aoc.testCase(isViablePasscode2, [112233,],  true);
+        aoc.testCase(isViablePasscode2, [123444,], false);
+        aoc.testCase(isViablePasscode2, [111122,],  true);
 
         document.querySelector("#testResults").innerHTML = "All tests passed!";
     };
@@ -45,10 +98,16 @@ let aoc04 = function() {
     return {
         processFile: processFile,
         solvePart1: (range) => {
-            return countViablePasscodes(range);
+            return {
+                actual: countViablePasscodes(range, isViablePasscode1), 
+                expected: 530,
+            };
         },
         solvePart2: (range) => {
-            return "Beans";
+            return {
+                actual: countViablePasscodes(range, isViablePasscode2),
+                expected: 324,
+            };
         },
     };
 }();
