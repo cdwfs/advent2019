6c06aaaf6473585509f0d988fe808eace5837a09
Cort <1944792+cdwfs@users.noreply.github.com>
2019-12-06 15:52:08 -0800

    Day 6 solution

--- day06/day06.js
+++ day06/day06.js
@@ -10,18 +10,60 @@ let aoc06 = function() {
         }
     };
 
-    let countOrbits = function(pairs) {
-        let bodies = {
-            COM: { depth: 0, },
-        };
+    const setDepth = function(orbitTree, bodyDepths, body, depth) {
+        if (bodyDepths.hasOwnProperty(body)) {
+            throw `${body} already present in depth array`;
+        }
+        bodyDepths[body] = depth;
+        orbitTree[body].children.forEach( child => {
+            setDepth(orbitTree, bodyDepths, child, depth+1);
+        });
+    };
+
+    let buildOrbitTree = function(pairs) {
+        let orbitTree = {};
         pairs.forEach( (pair) => {
             const parent = pair.parent;
             const child = pair.child;
-            if (!bodies.hasOwnProperty(parent)) {
-                bodies[parent] = {};
+            if (!orbitTree.hasOwnProperty(parent)) {
+                orbitTree[parent] = {children: [],};
             }
+            if (!orbitTree.hasOwnProperty(child)) {
+                orbitTree[child] = {children: [],};
+            }
+            orbitTree[parent].children.push(child);
+            orbitTree[child].parent = parent;
         });
-        return 0;
+        return orbitTree;
+    };
+    
+    let countOrbits = function(pairs) {
+        const orbitTree = buildOrbitTree(pairs);
+        
+        let bodyDepths = {};
+        setDepth(orbitTree, bodyDepths, "COM", 0);
+        return Object.entries(bodyDepths).reduce( (sum, entry) => sum + entry[1], 0);
+    };
+
+    let shortestPathLength = function(pairs) {
+        const orbitTree = buildOrbitTree(pairs);
+
+        let pathToRoot = function(body) {
+            let path = [];
+            let b = body;
+            while (b !== "COM") {
+                path.push(b);
+                b = orbitTree[b].parent;
+            }
+            return path;
+        };
+        let pathYOU = pathToRoot("YOU");
+        let pathSAN = pathToRoot("SAN");
+        while(pathYOU[pathYOU.length-1] === pathSAN[pathSAN.length-1]) {
+            pathYOU.pop();
+            pathSAN.pop();
+        }
+        return pathYOU.length + pathSAN.length - 2;
     };
     
     let processFile = function(inElem, callback, outElem) {
@@ -31,13 +73,17 @@ let aoc06 = function() {
             //const file = event.target.result;
             const allLines = reader.result.split(/\r\n|\n/u);
             const re = /(\w+)\)(\w+)/u;
-            const match = allLines[0].match(re);
-            let count = 0;
-            if (match) {
-                count = callback({parent: match[1], child: match[2],});
-            }
+            const pairs = allLines.map( line => {
+                const match = line.match(re);
+                if (match) {
+                    return {parent: match[1], child: match[2],};
+                } else {
+                    return "BLURG";
+                }
+            });
+            const result = callback(pairs);
 
-            document.querySelector(outElem).innerHTML = count;
+            document.querySelector(outElem).innerHTML = result;
         };
         reader.onerror = (event) => {
             alert(event.target.error.name);
@@ -60,17 +106,34 @@ let aoc06 = function() {
             {parent:"J",child:"K",},
             {parent:"K",child:"L",},
         ], 42);
+        // Part 2 tests
+        testCase(shortestPathLength, [
+            {parent:"COM",child:"B",},
+            {parent:"B",child:"C",},
+            {parent:"C",child:"D",},
+            {parent:"D",child:"E",},
+            {parent:"E",child:"F",},
+            {parent:"B",child:"G",},
+            {parent:"G",child:"H",},
+            {parent:"D",child:"I",},
+            {parent:"E",child:"J",},
+            {parent:"J",child:"K",},
+            {parent:"K",child:"L",},
+            {parent:"K",child:"YOU",},
+            {parent:"I",child:"SAN",},
+        ], 4);
         
         document.querySelector("#testResults").innerHTML = "All tests passed!";
     };
     
     return {
         processFile: processFile,
-        solvePart1: (range) => {
-            return countViablePasscodes(range);
+        solvePart1: (pairs) => {
+            return countOrbits(pairs);
         },
-        solvePart2: (range) => {
-            return "Beans";
+        solvePart2: (pairs) => {
+            return shortestPathLength(pairs);
         },
     };
 }();
+ 
