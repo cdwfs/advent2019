f74c9eac468ae63fee9cacb3c7bf21a4d5d8101a
Cort Stratton <cort@unity3d.com>
2019-12-18 16:29:18 -0800

    Day 18 part 1 WIP

--- day18/day18.js
+++ day18/day18.js
@@ -4,7 +4,44 @@ let aoc18 = function() {
 
     function parseInput(text) {
         const allLines = text.split(/\r\n|\n/u).filter(line => line !== "");
-        return allLines[0];
+        let map = {
+            grid: [],
+        };
+        allLines.forEach(line => {
+            map.grid.push(line.split(""));
+        });
+        map.height = map.grid.length;
+        map.width = map.grid[0].length;
+        map.stepCount = 0;
+        // Local keys, doors, and you
+        map.keyPos = {};
+        map.doorPos = {};
+        map.foundKeys = [];
+        for(let y=0; y<map.height; ++y) {
+            for(let x=0; x<map.width; ++x) {
+                const c = map.grid[y][x];
+                if (c === "#" || c === ".") {
+                    continue;
+                } else if (c === "@") {
+                    map.youPos = [x,y,];
+                } else if ("a" <= c && c <= "z") {
+                    map.keyPos[c] = [x,y,];
+                } else if ("A" <= c && c <= "Z") {
+                    map.doorPos[c] = [x,y,];
+                }
+            }
+        }
+        return map;
+    }
+
+    function cloneMap(map) {
+        return JSON.parse(JSON.stringify(map));
+    }
+    
+    function printMap(map) {
+        map.grid.forEach( line => {
+            console.log(line.join(""));
+        });        
     }
     
     function processFile(inElem, callback, outElem) {
@@ -13,8 +50,8 @@ let aoc18 = function() {
         reader.onload = (event) => {
             //const file = event.target.result;
             const fileContents = reader.result;
-            const bodies = parseInput(fileContents);
-            const result = callback(bodies);
+            const map = parseInput(fileContents);
+            const result = callback(map);
             if (result.actual === result.expected) {
                 document.querySelector(outElem).innerHTML = `${result.actual} (Correct!)`;
             } else {
@@ -27,6 +64,88 @@ let aoc18 = function() {
         reader.readAsText(firstFile);
     }
 
+    function minStepsToGetAllKeys(map) {
+        let pq = aoc.createPriorityQueue((a,b) => a.stepCount < b.stepCount);
+        pq.push(map);
+        while(!pq.empty()) {
+            // Pop the state with the shortest total path length.
+            let state = pq.pop();
+            console.log(`After ${state.stepCount} steps, found keys [${state.foundKeys}]`);
+            // If this state has no keys left, we're done; 
+            if (Object.keys(state.keyPos).length === 0) {
+                return state.stepCount;
+            }
+            // Find shortest path to all reachable keys. Just flood-fill with djikstra until that proves too slow.
+            let stepsToKey = {};
+            const [yx,yy] = state.youPos;
+            let visitedHashes = {};
+            let toVisit = [
+                [yx,yy,0,],
+            ];
+            while(toVisit.length > 0) {
+                const [px,py,steps] = toVisit.shift();
+                // Skip if we've been here already
+                const hash = py*state.width + px;
+                if (visitedHashes.hasOwnProperty(hash)) {
+                    continue;
+                }
+                visitedHashes[hash] = 1;
+                // If the current cell is a key, record the step count
+                const pc = state.grid[py][px];
+                if ("a" <= pc && pc <= "z") {
+                    console.assert(!stepsToKey.hasOwnProperty(pc), `Already have a shortest path to key ${pc}`);
+                    stepsToKey[pc] = steps;
+                    //console.log(`${steps} steps from ${yx},${yy} to key ${pc} at ${px},${py}`);
+                    continue;
+                }
+                // Add passable neighbors to the toVisit list
+                const neighbors = [ [px+1,py,], [px-1,py,], [px,py+1,], [px,py-1,], ];
+                for(const [nx,ny] of neighbors) {
+                    if (nx < 0 || nx >= state.width || ny < 0 || ny >= state.height) {
+                        continue;
+                    }
+                    const nc = state.grid[ny][nx];
+                    if (nc === "#") {
+                        continue; // wall
+                    } else if ("A" <= nc && nc <= "Z") {
+                        continue; // door
+                    } else {
+                        toVisit.push([nx,ny,steps+1,]); // empty cell, key, you
+                    }
+                }
+            }
+            // For each reachable key:
+            //printMap(state);
+            for(const [key,steps] of Object.entries(stepsToKey)) {
+                console.assert(state.keyPos.hasOwnProperty(key), `Key doesn't exist in map?`);
+                const [kx,ky] = state.keyPos[key];
+                // - Create a new map state in which we've moved to that key, picked it up, and unlocked (removed) the corresponding door.
+                let newState = cloneMap(state);
+
+                newState.stepCount += steps;
+
+                newState.grid[yy][yx] = ".";
+
+                console.assert(state.grid[ky][kx] === key, `Expected to find key ${key} here!`);
+                newState.grid[ky][kx] = "@";
+                newState.youPos = [kx,ky,];
+                delete newState.keyPos[key];
+                newState.foundKeys.push(key);
+                
+                const door = key.toUpperCase();
+                if (state.doorPos.hasOwnProperty(door)) {
+                    const [dx,dy] = state.doorPos[door];
+                    console.assert(state.grid[dy][dx] === door, `Expected to find door ${door} here!`);
+                    newState.grid[dy][dx] = ".";
+                    delete newState.doorPos[door];
+                }
+                // Otherwise, add it to the list to visit later.
+                pq.push(newState);
+            }
+        }
+        throw `Ran out of map states & didn't find all keys?!?`;
+    }
+    
     window.onload = function() {
         // priority queue tests
         let pq = aoc.createPriorityQueue((a,b) => a > b);
@@ -42,16 +161,59 @@ let aoc18 = function() {
         }
         aoc.testCase(pq.empty, [], true);
         // part 1
+        let text = `\
+#########
+#b.....@#
+#########`;
+        aoc.testCase(minStepsToGetAllKeys, [parseInput(text),], 6);
+
+        text = `\
+########################
+#f.D.E.e.C.b.A.@.a.B.c.#
+######################.#
+#d.....................#
+########################`;
+        aoc.testCase(minStepsToGetAllKeys, [parseInput(text),], 86);
+
+        text = `\
+########################
+#...............b.C.D.f#
+#.######################
+#.....@.a.B.c.d.A.e.F.g#
+########################`;
+        aoc.testCase(minStepsToGetAllKeys, [parseInput(text),], 132);
+
+        text = `\
+#################
+#i.G..c...e..H.p#
+########.########
+#j.A..b...f..D.o#
+########@########
+#k.E..a...g..B.n#
+########.########
+#l.F..d...h..C.m#
+#################`;
+        //aoc.testCase(minStepsToGetAllKeys, [parseInput(text),], 136);
+
+        text = `\
+########################
+#@..............ac.GI.b#
+###d#e#f################
+###A#B#C################
+###g#h#i################
+########################`;
+        aoc.testCase(minStepsToGetAllKeys, [parseInput(text),], 81);
+        
         // part 2
         document.querySelector("#testResults").innerHTML = "All tests passed!";
     };
     
     return {
         processFile: processFile,
-        solvePart1: (signal) => {
+        solvePart1: (map) => {
             return {
-                actual: testFFT(signal, 100),
-                expected: "84970726",
+                actual: minStepsToGetAllKeys(map),
+                expected: 17,
             };
         },
         solvePart2: (signal) => {
