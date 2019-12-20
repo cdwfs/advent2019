63f5f86ddeaef33bbd8a0262eee2f8a0a5525d7c
Cort <1944792+cdwfs@users.noreply.github.com>
2019-12-19 22:07:31 -0800

    Day 20 skeleton

---
+++ day20/day20.js
@@ -0,0 +1,118 @@
+/* global aoc */
+let aoc20 = function() {
+    "use strict";
+
+    function parseInput(text) {
+        const allLines = text.split(/\r\n|\n/u).filter(line => line !== "");
+        const mapWidth = allLines[0].length - 4;
+        const mapHeight = allLines.length - 4;
+        
+        return {
+            width: mapWidth,
+            height: mapHeight,
+        };
+    }
+
+    function processFile(inElem, callback, outElem) {
+        let firstFile = document.querySelector(inElem).files[0];
+        let reader = new FileReader();
+        reader.onload = (event) => {
+            //const file = event.target.result;
+            const fileContents = reader.result;
+            const map = parseInput(fileContents);
+            const result = callback(map);
+            if (result.actual === result.expected) {
+                document.querySelector(outElem).innerHTML = `${result.actual} (Correct!)`;
+            } else {
+                document.querySelector(outElem).innerHTML = `${result.actual} (ERROR: expected ${result.expected})`;
+            }
+        };
+        reader.onerror = (event) => {
+            alert(event.target.error.name);
+        };
+        reader.readAsText(firstFile);
+    }
+
+    window.onload = function() {
+        // part 1
+        let text = `\
+         A           
+         A           
+  #######.#########  
+  #######.........#  
+  #######.#######.#  
+  #######.#######.#  
+  #######.#######.#  
+  #####  B    ###.#  
+BC...##  C    ###.#  
+  ##.##       ###.#  
+  ##...DE  F  ###.#  
+  #####    G  ###.#  
+  #########.#####.#  
+DE..#######...###.#  
+  #.#########.###.#  
+FG..#########.....#  
+  ###########.#####  
+             Z       
+             Z       `;
+        aoc.testCase(shortestSolutionLength, [parseInput(text),], 23);
+
+        let text = `\
+                   A               
+                   A               
+  #################.#############  
+  #.#...#...................#.#.#  
+  #.#.#.###.###.###.#########.#.#  
+  #.#.#.......#...#.....#.#.#...#  
+  #.#########.###.#####.#.#.###.#  
+  #.............#.#.....#.......#  
+  ###.###########.###.#####.#.#.#  
+  #.....#        A   C    #.#.#.#  
+  #######        S   P    #####.#  
+  #.#...#                 #......VT
+  #.#.#.#                 #.#####  
+  #...#.#               YN....#.#  
+  #.###.#                 #####.#  
+DI....#.#                 #.....#  
+  #####.#                 #.###.#  
+ZZ......#               QG....#..AS
+  ###.###                 #######  
+JO..#.#.#                 #.....#  
+  #.#.#.#                 ###.#.#  
+  #...#..DI             BU....#..LF
+  #####.#                 #.#####  
+YN......#               VT..#....QG
+  #.###.#                 #.###.#  
+  #.#...#                 #.....#  
+  ###.###    J L     J    #.#.###  
+  #.....#    O F     P    #.#...#  
+  #.###.#####.#.#####.#####.###.#  
+  #...#.#.#...#.....#.....#.#...#  
+  #.#####.###.###.#.#.#########.#  
+  #...#.#.....#...#.#.#.#.....#.#  
+  #.###.#####.###.###.#.#.#######  
+  #.#.........#...#.............#  
+  #########.###.###.#############  
+           B   J   C               
+           U   P   P               `;
+        aoc.testCase(shortestSolutionLength, [parseInput(text),], 58);
+        // part 2
+        document.querySelector("#testResults").innerHTML = "All tests passed!";
+    };
+    
+    return {
+        processFile: processFile,
+        solvePart1: (map) => {
+            return {
+                actual: minStepsToGetAllKeys(map),
+                expected: 17,
+            };
+        },
+        solvePart2: (signal) => {
+             return {
+                actual: testFFTAtOffset(signal, 100),
+                expected: "47664469",
+            };
+        },
+    };
+}();
